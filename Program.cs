81b55cd0220b70bb9bbacbb0f94236c790dca23b
sik-213003 <145955613+sik-213003@users.noreply.github.com>
2023-10-29 18:13:19 +0500

    Program.cs Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+namespace MyWinFormsApp;
+
+static class Program{
+    static void Main()
+    {
+
+        MessageBox.Show("Hello World", "Test", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+    }    
+}
