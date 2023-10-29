cfc5f9da93012a97051e6cca0192b88e4034740a
sik-213003 <145955613+sik-213003@users.noreply.github.com>
2023-10-29 18:08:24 +0500

    Initial Commit

---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace MyWinFormsApp;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        MessageBox.Show("Hello World", "test", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+    }
+}
