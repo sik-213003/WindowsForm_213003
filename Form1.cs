262f90f2c145de176f5087788f5674cc168c8877
sik-213003 <145955613+sik-213003@users.noreply.github.com>
2023-10-29 18:12:33 +0500

    Delete Form1.cs

--- Form1.cs
+++
@@ -1,9 +0,0 @@
-namespace MyWinFormsApp;
-
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        MessageBox.Show("Hello World", "test", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-    }
-}
